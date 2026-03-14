8c02e4874b8c4a3285b0311db4dc61ae6beea8ac
Edú Bladimir <edubladimir.dev@gmail.com>
2026-03-13 23:31:36 -0500

    Update .gitignore to include additional entries for Maven, IntelliJ IDEA, VSCode, OS files, and Spring Boot logs

---
+++ src/main/java/com/platzi/devops/api/CursoDevopsApiApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.devops.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoDevopsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoDevopsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/platzi/devops/api/CursoDevopsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.devops.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDevopsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
